41a2725fa354383f8d1101cc06f1cb34b17f102c
Douglas Balkin <dbalkin77@gmail.com>
2017-08-16 13:26:51 -0500

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Krusty Burger');
+INSERT INTO burgers (burger_name) VALUES ('Krusty Burger with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Deep Fried Krusty Burger');
